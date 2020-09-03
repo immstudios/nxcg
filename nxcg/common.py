2588584262cf84aa8bd4b432fb78df2627f54439
Martastain <martas@imm.cz>
2020-09-03 23:23:14 +0200

    Added PIL support, image2surface, surface2image

--- nxcg/common.py
+++ nxcg/common.py
@@ -1,6 +1,8 @@
 import os
 import array
 
+from nxtools import *
+
 import gi
 import cairo
 
@@ -9,4 +11,31 @@ gi.require_version('PangoCairo', '1.0')
 
 from gi.repository import Pango, PangoCairo
 
-from nxtools import *
+try:
+    import PIL
+except ImportError:
+    logging.warning("PIL is not available")
+
+
+def surface2image(surface):
+    return PIL.Image.frombuffer(
+            "RGBA",
+            (surface.get_width(), surface.get_height()),
+            surface.get_data(),
+            "raw",
+            "RGBA",
+            0,
+            1
+        )
+
+def image2surface(image):
+    w, h = image.size
+    stride = w * 4
+    img_bytes = image.tobytes()
+    return cairo.ImageSurface.create_for_data(
+            array.array('B',img_bytes),
+            cairo.FORMAT_ARGB32,
+            w, 
+            h, 
+            stride
+        )
